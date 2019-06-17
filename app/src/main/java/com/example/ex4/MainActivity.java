186181d5c096840562bdbed35ad1cceab26a8174
David Goldberg <offirgold@gmail.com>
2019-06-17 14:25:41 +0300

    #second

--- app/src/main/java/com/example/ex4/MainActivity.java
+++ app/src/main/java/com/example/ex4/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ex4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickConnect(View view) {
+        EditText editTextIp = findViewById(R.id.ip_edit_text);
+        EditText editTextPort = findViewById(R.id.port_edit_text);
+
+        String ip = editTextIp.getText().toString();
+        String port = editTextPort.getText().toString();
+
+        connectToServer(ip, port);
+    }
+
+    private void connectToServer(String ip, String port){
+
+
+
+    }
+
 }
